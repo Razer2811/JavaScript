b7563afac4e730eec0a06a3abf7b1c16143626b5
MinhKhanh <tranleminhkhanh5734@gmail.com>
2026-06-30 08:59:30 +0700

    lession 1

--- lession_1/main.js
+++ lession_1/main.js
@@ -1,1 +1,7 @@
-alert("Hello word, we are here JavaScript")
+//Cách khai báo biến
+var fullName = "Tran Le Minh Khanh";
+var age = 21;
+console.log(fullName);
+confirm("Do you have 18+ !!!")
+//Cú pháp comments
+
