9c7b77709326264fba260153ebe0671e5241e6a7
MinhKhanh <tranleminhkhanh5734@gmail.com>
2026-06-30 09:58:41 +0700

    lession 2 70%

---
+++ lession_2/main.js
@@ -0,0 +1,44 @@
+var age = 20
+var nextAge = age+1;
+console.log(nextAge);
+// toán tử số học
+var a = 4
+var b = -5
+var c = a + b
+var d = a - b
+console.log(c)
+//toán tử tiền tố hậu tố
+var number = 1;
+number++;
+console.log(number)
+number++;
+console.log(number)
+
+
+// Toán tử chuỗi ~ String operator
+var firstName = 'Tran'
+var lastName = 'Khanh'
+var fullName = firstName + ' ' + lastName
+console.log(fullName);
+
+
+// Kiểu dữ liệu Boolean
+
+var age = 16
+var canBuyAlcohol = age >= 18
+
+console.log(canBuyAlcohol)
+
+// Câu lệnh If-else
+// ví dụ số chẵn lẻ
+var number1 = 13
+var remain = number1 % 2;
+var isOdd = remain == 1;
+console.log(isOdd)
+
+// Toán tử logical
+var h = true
+var j = false
+var f = h || j
+var k = h && j
+console.log(f,k);
