715a323e0b8d29f024ee668331083050d0b60041
MinhKhanh <tranleminhkhanh5734@gmail.com>
2026-07-06 16:24:03 +0700

    finish lession 10

--- lesson_10.2/main.js
+++ lesson_10.2/main.js
@@ -1,3 +1,5 @@
+// Map() trong mảng
+
 const listSports = [
     { name: 'Bóng đá', like: 9 },
     { name: 'Bóng rổ', like: 4 },
@@ -15,3 +17,41 @@ function sportInsert(sport){
 var newSport = listSports.map(sportInsert);
 
 console.log(newSport);
+
+// Reduce() trong Mảng
+
+var totalLike = listSports.reduce(function likeHandler(accumulator, currentValue) {
+    return accumulator + currentValue.like;
+}, 0); // initial value : giá trị khởi đầu là không bắt buột
+
+console.log(totalLike);
+
+
+// Ví dụ Array reduce() method
+// Flat - làm phẳng mảng từ mảng xâu : deptArray
+var deptFlat = [1,2,[3,4],5,6,[7,8,9]]
+
+var flatArray = deptFlat.reduce(function(flatOutput, deptindex){
+    return flatOutput.concat(deptindex);
+}, []);
+
+console.log(flatArray);
+
+// Bài tập về reduce()
+
+
+ 
+// Expected results:
+var arr = [
+    ['name', 'Sơn Đặng'],
+    ['age', 18],
+];
+
+function arrToObj(arr){
+    return arr.reduce(function(obj, item){
+        obj[item[0]] = item[1]
+        return obj;
+    }, {})
+}
+
+console.log(arrToObj(arr)); // { name: 'Sơn Đặng', age: 18 }
