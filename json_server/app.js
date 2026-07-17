d28d9b2d21385a4485674beb5f2de740814aeaf7
MinhKhanh <tranleminhkhanh5734@gmail.com>
2026-07-17 23:33:51 +0700

    tiep tuc JSON

--- json_server/app.js
+++ json_server/app.js
@@ -1,11 +1,16 @@
-
-
 var courseApi = 'http://localhost:3000/course';
 
 fetch(courseApi)
-    .then(function(response){
+    .then(function (response) {
+        if (!response.ok) {
+            throw new Error('Lỗi HTTP: ' + response.status);
+        }
+
         return response.json();
     })
-    .then(function(courses){
+    .then(function (courses) {
         console.log(courses);
     })
+    .catch(function (error) {
+        console.error('Không lấy được courses:', error);
+    });
